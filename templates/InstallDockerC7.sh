2ec8ed3c57309cd2a67429722a365431e89f47a7
pmbibe <53257014+pmbibe@users.noreply.github.com>
2020-06-11 16:27:00 +0700

    Add files via upload

---
+++ templates/InstallDockerC7.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+sudo yum remove docker \
+                  docker-client \
+                  docker-client-latest \
+                  docker-common \
+                  docker-latest \
+                  docker-latest-logrotate \
+                  docker-logrotate \
+                  docker-engine
+sudo yum install -y yum-utils  
+sudo yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install docker-ce docker-ce-cli containerd.io -y
+sudo systemctl start docker
+sudo systemctl enable docker
+echo "Docker has been installed"
+cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOF
+sudo yum install -y kubelet kubeadm kubectl
+systemctl enable kubelet
+systemctl start kubelet
+echo "K8S has been installed"
