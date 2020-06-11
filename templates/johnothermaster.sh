bab7da0b6c716ba667efe5abf2dbc11519130443
pmbibe <53257014+pmbibe@users.noreply.github.com>
2020-06-11 16:33:00 +0700

    Add files via upload

---
+++ templates/johnothermaster.sh
@@ -0,0 +1,2 @@
+!#/bin/bash
+kubeadm join 192.168.141.230:6443 --token xf06gu.qiv2xp7lju8zrzw7     --discovery-token-ca-cert-hash sha256:d281eb9182b287c81cb607746138e24ac538f2706bcd2e48b6a1a7e9c62da6c7  --control-plane
